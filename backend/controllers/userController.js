8fd6200115e34dbdecc94309c4728bcf8298c320
Abhishek <147073711+CodewithAbhi14@users.noreply.github.com>
2024-12-01 04:06:30 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET || abhishek, { expiresIn: '1h' });
     res.json({ token,
       role: user.role,
      });
